5cb1fd47c040efa18d6721a82ce54a193639ab33
Lee Berg <leealanberg@gmail.com>
2019-05-28 23:31:18 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# BlueUDMotion
-A Universal Dashboard for the "Motion Project"
+# BlueUDMotion 🎥 🐈 🐕
+
+**Blue Motion** is a simple dashboard front-end for [Motion](https://motion-project.github.io/) using the free Community Edition of [PowerShell Universal Dashboard](https://universaldashboard.io/) from [Ironman Software](https://github.com/adamdriscoll)
+
+![Demo GIF](./img/gifdemo.gif)
+
+## Features
+
+1. Most of the basic Motion Web UI presented on a Dashboard - including the Live View and snapshot and motion detection controls.
+2. Preview "Gallery" of captured images with "easy download"
+
+## Setup
+
+This setup assumes a Windows Machine running UD and a file share, and a Raspberry Pi running a basic Motion configuration...
+
+1. Setup a Windows File Share on your Universal Dashboard Sever - Motion will deliver output to this folder and Universal Dashboard will publish this folder to the web interface.
+2. Setup [PowerShell Universal Dashboard](https://universaldashboard.io/)
+3. Mount the Share on Raspbian
+  ``` bash
+  sudo apt-get install cifs-utils
+  sudo mkdir /mtn/SpyPics
+  sudo mount -t cifs //YOURUDSERVER/Share/BlueUD /mnt/SpyPics -o user=username,pass=password,dir_mode=0777,file_mode=0777
+  ```
+4. Raspbian Setup: (More Detailed Steps Soon...)
+    * Ensure the Camera is Enabled.
+    * Install Motion
+    * Configure Motion
+      ``` bash 
+      sudo nano /etc/motion/motion.conf
+      ```
+5. Run the UD Dashboard
+6. Do some test Shots
+7. Capture picture of your cats!
+![](./img/cat1.png)
+
+## Note
+This project was created to help me figure out which cat was taking a dump in my basement - Motion is great software please support that project and do cool things with it!
---
+++ start.ps1
@@ -0,0 +1,116 @@
+﻿# BlueUDMotion 
+
+# TODO : Pages and Functions on ONE Page? pssh - sloppy...
+
+# Common Variables
+
+# Your Motion API - TODO: Auth LOL!
+$Cache:MotionManagementURL = 'http://192.168.99.168'
+# Motion PORT 
+$Cache:MotionManagementPort = '8080'
+# UD Server should have a share receiving Motion Captures - this will be expoxed by the webserver.
+$Cache:PictureSharePath = 'C:\Share\MotionDumpFolder'
+
+
+Function Get-CameraShareContents
+{
+
+	Try{
+
+		$ChildItems = Get-ChildItem -Path $Cache:PictureSharePath
+		return $ChildItems
+
+	}
+	Catch{
+	
+		return $null
+	
+	}
+	
+}
+
+
+$HomePage = New-UDPage -Name "Home" -Icon home -Endpoint {
+
+	New-UDRow -Columns {
+
+		New-UDColumn {
+			New-UDImage -Id "img_LivePreview" -Url ($Cache:MotionManagementURL + ':8081/') -Height 720 -Width 1280
+		}
+		
+	}
+
+	New-UDRow -Columns {
+
+		New-UDColumn {
+
+			New-UDButton -Id "btn_TakeSnap" -Text "Take Snapshot" -Icon camera -OnClick {
+				$REQ = Invoke-WebRequest -Uri ($Cache:MotionManagementURL + ':' + $Cache:MotionManagementPort + '/0/action/snapshot') -UseBasicParsing
+				New-UDInputAction -Toast "Taking SnapShot!" -Duration 10
+			}
+			New-UDButton -Id "btn_MakeMovie" -Text "Take Video" -Icon camera_retro -OnClick {
+				$REQ = Invoke-WebRequest -Uri ($Cache:MotionManagementURL + ':' + $Cache:MotionManagementPort + '/0/action/makemovie') -UseBasicParsing
+				New-UDInputAction -Toast "Taking Video!" -Duration 10
+			}
+			New-UDButton -Id "btn_PauseMotionDetection" -Text "Pause Motion Detection" -Icon pause -OnClick {
+				$REQ = Invoke-WebRequest -Uri ($Cache:MotionManagementURL + ':' + $Cache:MotionManagementPort + '/0/detection/pause') -UseBasicParsing
+				New-UDInputAction -Toast "Stopping Motion Detection!" -Duration 10
+			}
+			New-UDButton -Id "btn_StartMotionDetection" -Text "Resume Motion Detection" -Icon play -OnClick {
+				$REQ = Invoke-WebRequest -Uri ($Cache:MotionManagementURL + ':' + $Cache:MotionManagementPort + '/0/detection/start') -UseBasicParsing
+				New-UDInputAction -Toast "Starting Motion Detection!" -Duration 10
+			}
+		}
+
+	}
+	
+}
+
+
+$FilePage = New-UDPage -Name "Downloads" -Icon download -Content {
+	
+	
+	New-UdGrid -Id 'grd_Files' -Title "Files" -Headers @("Name","LastWriteTime","Preview","Open") -Properties @("Name","LastWriteTime","Preview","Open") -Endpoint {
+
+		$Files = Get-CameraShareContents
+
+		$Files | Sort-Object -Property LastWriteTime -Descending | ForEach-Object{
+
+			$ImageFile = 'Downloads/' + $_.Name
+
+			[PSCustomObject]@{
+				Name = $_.BaseName
+				LastWriteTime = $_.LastWriteTime
+				Preview = New-UDMuAvatar -Image $ImageFile -Alt 'preview' -Style @{width = 128; height = 72; borderRadius = '1px'}
+				Open = New-UDButton -Text "Download" -OnClick (New-UDEndpoint -Endpoint{
+						
+					$FilePath = $ArgumentList[0]
+					$FileName = $ArgumentList[1]
+
+					# Get Image
+					Copy-Item -Path $FilePath -Destination $Cache:DownloadsFolder -Force
+
+					#$FullPathWeb = $Cache:DownloadsFolder + '/' + $FileName
+					#$#FullPathWeb = $FullPathWeb.Replace('\',"/")
+
+					Invoke-UDRedirect -Url ('Downloads/' + $FileName) -OpenInNewWindow
+	
+				} -ArgumentList $_.FullName, $_.Name)
+			}
+		
+		} | Out-UDGridData
+	}
+	
+}
+
+Get-UDDashboard | Stop-UDDashboard
+
+$Pages = @($HomePage, $FilePage)
+
+$Endpoints = New-UDEndpointInitialization -Function @("Get-CameraShareContents")
+
+$DownloadsFolder = Publish-UDFolder -Path ($Cache:PictureSharePath+'\') -RequestPath '/Downloads'
+
+$Dashboard = New-UDDashboard -Title "BlueUDMotion 🎥 🐈 🐕" -Pages $Pages -EndpointInitialization $Endpoints
+
+Start-UDDashboard -Dashboard $Dashboard -Port 10000 -PublishedFolder $DownloadsFolder🐕
